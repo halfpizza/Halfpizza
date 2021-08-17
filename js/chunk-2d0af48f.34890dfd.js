2b3e1ae789c52cffcdefdfade750af3c7caeb20a
HalfPizza <85058030+halfpizza@users.noreply.github.com>
2021-08-17 20:53:22 +0800

    Add files via upload

---
+++ js/chunk-2d0af48f.34890dfd.js
@@ -0,0 +1,6 @@
+/*!
+ *  build: admin-pro 
+ *  copyright: chuzhixin 1204505056@qq.com 
+ *  time: 2021-8-12 23:21:55
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af48f"],{"0e59":function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"nft-container"},[e("div",{staticStyle:{color:"#f07084","font-size":"30px"}},[t._v(" "+t._s(t.translateTitle("即将开放敬请期待"))+" ")])])},s=[],c=e("79fa"),i={name:"Nft",components:{},data:function(){return{}},created:function(){},mounted:function(){},methods:{translateTitle:c["translateTitle"]}},l=i,o=e("2877"),r=Object(o["a"])(l,a,s,!1,null,null,null);n["default"]=r.exports}}]);
---
+++ js/chunk-2d217aaa.3e87ad1f.js
@@ -0,0 +1,6 @@
+/*!
+ *  build: admin-pro 
+ *  copyright: chuzhixin 1204505056@qq.com 
+ *  time: 2021-8-12 23:21:55
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217aaa"],{c865:function(a,t,e){"use strict";e.r(t);var n=function(){var a=this,t=a.$createElement,e=a._self._c||t;return e("div",{staticClass:"teaching-container"},[e("el-row",{attrs:{gutter:10}},[e("el-col",{attrs:{span:24}},[e("vab-player-mp4",{attrs:{config:a.mp4Config}})],1)],1)],1)},c=[],l=e("34d4"),r={name:"Teaching",components:{VabPlayerMp4:l["VabPlayerMp4"]},data:function(){return{mp4Config:{id:"mse1",url:"https://halfpizza.com/tech.mp4",volume:.6,autoplay:!0,fluid:!0,screenShot:!1,pip:!1}}}},o=r,p=e("2877"),i=Object(p["a"])(o,n,c,!1,null,null,null);t["default"]=i.exports}}]);
